673cd5ee3741e70877e8d0f6010a51d735486efa
闲着的鱼 <zhao_xiaosen@qq.com>
2024-03-04 17:41:26 +0800

    clang-format

--- example/protoc/main.cpp
+++ example/protoc/main.cpp
@@ -1,13 +1,13 @@
-#include "example/protoc/user_data.pb.h" 
-#include "iostream" 
+#include "example/protoc/user_data.pb.h"
+#include "iostream"
 
-using namespace std; 
+using namespace std;
 
-int main(int argc, char const *argv[]) 
-{ 
-    data::users::UserProfile userProfile; 
-    userProfile.set_email("756633687@qq.com"); 
-    userProfile.set_id(123456); 
-    cout <<                                                        "user profile id: " << userProfile.id() << " email: " << userProfile.email() ; 
-    return 0; 
-} 
+int main(int argc, char const* argv[]) {
+  data::users::UserProfile userProfile;
+  userProfile.set_email("756633687@qq.com");
+  userProfile.set_id(123456);
+
+  cout << "user profile id: " << userProfile.id() << " email: " << userProfile.email();
+  return 0;
+}
